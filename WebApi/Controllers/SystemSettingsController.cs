ccad4b55e9b4bbf063c041748cf0e08e3f153084
oilliever <baduy.work@gmail.com>
2023-11-10 19:13:33 +0700

    add system settings api

---
+++ WebApi/Controllers/SystemSettingsController.cs
@@ -0,0 +1,44 @@
+﻿using Application.SystemSettings;
+using Microsoft.AspNetCore.Mvc;
+using Models.SystemSettings;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("systemSettings")]
+    public class SystemSettingsController : Controller
+    {
+        private readonly ISystemSettingService _systemSettingsService;
+
+        public SystemSettingsController(ISystemSettingService systemSettingService)
+        {
+            _systemSettingsService = systemSettingService;
+        }
+
+        /// <summary>
+        /// Gets the list of all settings.
+        /// </summary>
+        [HttpGet("getSettings")]
+        public async Task<IActionResult> GetSettings()
+        {
+            var dish = await _systemSettingsService.GetSettings();
+            return Ok(dish);
+        }
+
+        /// <summary>
+        /// Update the setting specified by Id
+        /// </summary>
+        [HttpPut("put/{id}")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Update([FromRoute] string id, [FromForm] SystemSettingUpdateRequest req)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _systemSettingsService.Update(req);
+            if (result == 0)
+                return BadRequest();
+            return Ok();
+        }
+    }
+}
